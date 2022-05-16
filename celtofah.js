5f91a00d96867ee05de8d3c66251df19aa0feeef
Gabriel Francisco Xavier <91631321+gabsnf@users.noreply.github.com>
2022-05-16 13:48:04 -0300

    Add files via upload

---
+++ celtofah.js
@@ -0,0 +1,12 @@
+var celsius=36 + "C"
+var fahrenheint;
+
+function celsiusTofahrenheit(celsius){
+    let hoje = celsius.replace("C","")
+    console.log(hoje)
+    let multiplicacao=hoje * 1.8 + 32 
+    multiplicacao = multiplicacao + "F"
+    console.log(multiplicacao)
+    
+}
+celsiusTofahrenheit(celsius)
